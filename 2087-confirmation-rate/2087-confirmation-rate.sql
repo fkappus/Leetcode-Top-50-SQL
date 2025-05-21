c754535c9a35bfa21e27541ee43b22c8d8257892
Finn Kappus <finn.kappus@web.de>
2025-05-21 10:11:10 +0200

    Time: 634 ms (85.03%), Space: 0B (100%) - LeetHub

---
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+    s.user_id, 
+    ROUND(
+        AVG(IF(c.action = "confirmed", 1.00, 0)),2)as confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id
